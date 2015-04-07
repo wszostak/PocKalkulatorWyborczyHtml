1fa99de9bafe18d18a2b7653e12dda3d26f73d5a
Sebastian Celejewski <Sebastian.Celejewski@wp.pl>
2015-04-07 11:41:34 +0200

    Ustawienie proxy z powrotem na JBossa na DEV, zamiast na localhost

--- src/frontend/Gruntfile.js
+++ src/frontend/Gruntfile.js
@@ -5,7 +5,7 @@ var config = {
     app: 'src', //app sources
     dist: 'dist', // builded app
     livereloadPort: 35729,
-    backendProxy: 'localhost'
+    backendProxy: '52.1.164.93'
 };
 
 module.exports = function(grunt) {
@@ -261,7 +261,7 @@ module.exports = function(grunt) {
                     port: 8080,
                     https: false,
                     xforward: false,
-                    rewrite: {'^/backend/service/protocol': '/backend/service/protocol'}
+                    rewrite: {'^/backend/service/protocol': '/openpkw/service/protocol'}
                 }]
             
         },
