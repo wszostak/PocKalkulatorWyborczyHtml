94c7b3a34e30e296e1b421c8e9f1d58bb5140870
Sebastian Celejewski <sebastian.celejewski@wp.pl>
2015-04-09 06:44:48 +0200

    Przejście z powrotem na JBossa po wrzucie KW-U-2 na mastera

--- src/frontend/Gruntfile.js
+++ src/frontend/Gruntfile.js
@@ -5,7 +5,7 @@ var config = {
     app: 'src', //app sources
     dist: 'dist', // builded app
     livereloadPort: 35729,
-    backendProxy: '91.250.114.134'
+    backendProxy: '52.1.164.93'
 };
 
 try {
@@ -273,7 +273,7 @@ module.exports = function(grunt) {
                     port: 8080,
                     https: false,
                     xforward: false,
-                    rewrite: {'^(\/backend\/service\/protocol[/]{0,1})(.*)$': '/poc-backend/service/protocol/$2'}
+                    rewrite: {'^(\/backend\/service\/protocol[/]{0,1})(.*)$': '/openpkw/service/protocol/$2'}
                 }]
             
         },
